049c4c0b4dbc15c2a0b21f9b71c6067c051059d5
Touha Zohair <touha98@gmail.com>
2021-01-19 12:32:41 +0600

    beta 0.2

--- api/pack/models/pack.js
+++ api/pack/models/pack.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeDelete(params) {
+      const p1 = strapi.query("invoice-item").find({ pack: params._id });
+      const p2 = strapi.query("challan-item").find({ pack: params._id });
+      const [items1, items2] = await Promise.all([p1, p2]);
+      if (items1.length) {
+        throw new Error("Pack used in Invoice " + items1[0].invoice.index);
+      } else if (items2.length) {
+        throw new Error("Pack used in Challan " + items2[0].challan.index);
+      }
+    },
+  },
+};
